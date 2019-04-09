9697dce4ba67deba50347b95e44689b71f7d9edd
unknown <ricardo-valenca@hotmail.com>
2019-04-09 16:32:42 -0300

    create 'createComment' function, make initial implementation

---
+++ controller/comments functions.js
@@ -0,0 +1,43 @@
+import Campground from "../models/campgrounds";
+import Comment from "../models/comments";
+
+
+const createComment = async (req, res) => {
+	// lookup campground using ID
+	Campground.findById(req.params.id, (err, campground) => {
+		if (err) {
+			console.log("Something went wrong when adding a new comment");
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			// take what was sent through the form and add to database
+			Comment.create(req.body.comment, (err, comment) => {
+				if (err) {
+					req.flash("error", "Something went wrong when adding the comment");
+					console.log(err);
+				} else {
+					// 'req.user' takes information from the user 
+					// currently logged in
+					// add current user id to comment
+					comment.author.id = req.user._id;
+					// add current username to comment
+					comment.author.username = req.user.username;
+					comment.save();
+					// insert comment on campground
+					campground.comments.push(comment);
+					campground.save();
+					
+					req.flash("success", "New comment addded successfully");
+					console.log(comment);
+					// redirect to current campground page
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			});
+		}
+	});
+}
+
+
+export {
+	createComment
+}
--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,9 @@
 import express from "express";
 import Campground from "../models/campgrounds";
 import Comment from "../models/comments";
+import {
+	createComment
+} from "../controller/comments functions";
 import {checkCommentOwnership, checkLogin} from "../middleware/index";
 
 
@@ -29,38 +32,7 @@ router.get("/new", checkLogin, (req, res) => {
 
 // CREATE - insert comment in specific campground, if logged in
 router.post("/", checkLogin, (req, res) => {
-	// lookup campground using ID
-	Campground.findById(req.params.id, (err, campground) => {
-		if (err) {
-			console.log("Something went wrong when adding a new comment");
-			console.log(err);
-			res.redirect("/campgrounds");
-		} else {
-			// take what was sent through the form and add to database
-			Comment.create(req.body.comment, (err, comment) => {
-				if (err) {
-					req.flash("error", "Something went wrong when adding the comment");
-					console.log(err);
-				} else {
-					// 'req.user' takes information from the user 
-					// currently logged in
-					// add current user id to comment
-					comment.author.id = req.user._id;
-					// add current username to comment
-					comment.author.username = req.user.username;
-					comment.save();
-					// insert comment on campground
-					campground.comments.push(comment);
-					campground.save();
-					
-					req.flash("success", "New comment addded successfully");
-					console.log(comment);
-					// redirect to current campground page
-					res.redirect("/campgrounds/" + campground._id);
-				}
-			});
-		}
-	});
+	createComment(req, res);
 });
 
 
