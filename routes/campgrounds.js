29f6861154db84b24ba8beed8d051447f82ef054
unknown <ricardo-valenca@hotmail.com>
2019-04-09 20:21:52 -0300

    small typos on error messages

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -46,7 +46,7 @@ router.get("/:id/edit", checkCampgroundOwnership, (req, res) => {
 	// find the campground to edit, by id
 	Campground.findById(req.params.id, (err, foundCampground) => {
 		if (err) {
-			req.flash("error", "Could not find campground");
+			req.flash("error", "Could not find the campground");
 			console.log(err);
 			res.redirect("back");
 		} else {
@@ -79,7 +79,7 @@ router.get("/:id", (req, res) => {
 		// doesn't returns an error, but also doesn't returns a
 		// valid entry (like 'null')
 		if(err || !foundCampground){
-			req.flash("error", "Could not find campground");
+			req.flash("error", "Could not find the campground");
 			console.log(err);
 			res.redirect("back");
 		} else {
